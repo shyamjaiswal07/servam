74d856fc0524645a137f6bfb16bd2640d7810111
Raunak Sharma <raunakk572@gmail.com>
2022-10-03 16:57:09 +0530

    demo1

--- src/page-component/Product.jsx
+++ src/page-component/Product.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div>Product Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem consequuntur magni consectetur tempore temporibus? Adipisci sequi molestias corrupti numquam aut reprehenderit sunt? Inventore non molestiae iusto neque reiciendis incidunt repellendus?</div>
+    <div>Product </div>
   )
 }
 
