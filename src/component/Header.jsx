23a782ec4861afc0fd3e05d7477931a81b527c2e
Raunak Sharma <raunakk572@gmail.com>
2022-10-03 16:23:54 +0530

    nav section done

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import './Header.css'
 import Topheader from './Topheader';
 import SearchIcon from '@mui/icons-material/Search';
+import SportsMotorsportsIcon from '@mui/icons-material/SportsMotorsports';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import CleanHandsIcon from '@mui/icons-material/CleanHands';
+import DoNotStepIcon from '@mui/icons-material/DoNotStep';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import AddRoadIcon from '@mui/icons-material/AddRoad';
+
 
 const Header = () => {
   return (
@@ -16,19 +23,20 @@ const Header = () => {
      <li className="sub-menu-parent" tab-index="0">
        <a href="http://google.com">PRODUCTS</a>
        <ul className="sub-menu">
-         <li><a href="#">Sub Item 1</a></li>
-         <li><a href="#">Sub Item 2</a></li>
-         <li><a href="#">Sub Item 3</a></li>
-         <li><a href="#">Sub Item 4</a></li>
+         <li> <span><SportsMotorsportsIcon /></span> <a href="#"> Head Protection</a></li>
+         <li><span><VisibilityIcon /></span> <a href="#">Eye Protection</a></li>
+         <li><span><CleanHandsIcon /></span> <a href="#">Hand Protection</a></li>
+         <li><span><DoNotStepIcon /></span><a href="#">Feet Protection</a></li>
+         <li><span><LocalFireDepartmentIcon /></span><a href="#">Fire Protection</a></li>
+         <li><span><AddRoadIcon /></span><a href="#">Raod Protection</a></li>
        </ul>
      </li>
      <li className="sub-menu-parent buisness__link" tab-index="0">
        <a href="#">BUISNESS</a>
        <ul className="sub-menu">
-         <li><a href="#">Sub Item 1</a></li>
-         <li><a href="#">Sub Item 2</a></li>
-         <li><a href="#">Sub Item 3</a></li>
-         <li><a href="#">Sub Item 4</a></li>
+         <li><a href="#">Regional Channel Partners</a></li>
+         <li><a href="#">International Channel Partners</a></li>
+        
        </ul>
      </li>
      <li className="sub-menu-parent" tab-index="0">
