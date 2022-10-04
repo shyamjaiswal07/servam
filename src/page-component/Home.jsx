f2ab25f02e7b09dd2cf93f64e83b538fd003efaf
Shyam Jaiswal <106074037+shyamjaiswal07@users.noreply.github.com>
2022-10-05 00:17:42 +0530

    commited by shyam

--- src/page-component/Home.jsx
+++ src/page-component/Home.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import SwipeableTextMobileStepper from '../component/Crousal'
-import Mission from '../component/Mission'
+import React from "react";
+import SwipeableTextMobileStepper from "../component/Crousal";
+import "./PageStyles/Home.css";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Typography from "@mui/material/Typography";
+// import { CardActionArea } from "@mui/material";
+import youtube from "../images/youtube.png";
 
 const Home = () => {
   return (
     <>
-    <SwipeableTextMobileStepper />
-    < Mission />
+      <SwipeableTextMobileStepper />
+      <div className="card_style">
+        <div className="shy">
+          <div className="imgStyle">
+            <img src={youtube} alt="" />
+          </div>
+          <div className="card-content">
+            <h1>heading</h1>
+            <p>
+              Lorem ipsum, dlore Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Cum illo nulla consectetur enim! Natus aliquam
+              maiores possimus aperiam veniam tenetur!
+            </p>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
